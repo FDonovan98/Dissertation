a00f883a028dc90ba26d2f86079dcdbbef723b80
HDonovan96 <hd241610@falmouth.ac.uk>
2022-01-27 11:40:45 +0000

    Include lines for twoway, interaction, & blocking anova tests

--- data_analysis/stat_analysis.r
+++ data_analysis/stat_analysis.r
@@ -1,7 +1,13 @@
 testData <- read.csv("data_analysis/parsed_supervisor_results.csv", header = TRUE, colClasses = c("factor", "factor", "numeric", "numeric"))
 
-summary(testData)
-
-oneway <- aov(buildConfidence ~ isExperimental + id, data = testData)
-
+oneway <- aov(buildConfidence ~ isExperimental, data = testData)
 summary(oneway)
+
+twoway <- aov(buildConfidence ~ isExperimental + id, data = testData)
+summary(twoway)
+
+interaction <- aov(buildConfidence ~ isExperimental * id, data = testData)
+summary(interaction)
+
+blocking <- aov(buildConfidence ~ isExperimental * id + team, data = testData)
+summary(blocking)
