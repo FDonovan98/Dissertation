f310a4255383a659ff8845df13d56e26d65c963c
HDonovan96 <hd241610@falmouth.ac.uk>
2022-02-15 13:37:52 +0000

    switched stats to run on student rather than supervisor data

--- data_analysis/stat_analysis.r
+++ data_analysis/stat_analysis.r
@@ -1,13 +1,23 @@
-testData <- read.csv("data_analysis/parsed_supervisor_results.csv", header = TRUE, colClasses = c("factor", "factor", "numeric", "numeric"))
+testData <- read.csv("data_analysis/parsed_student_results.csv", header = TRUE, colClasses = c("factor", "factor", "factor", "numeric", "numeric", "numeric", "numeric", "numeric"))
 
-oneway <- aov(buildConfidence ~ isExperimental, data = testData)
+oneway <- aov(scopeConfidence ~ isExperimental, data = testData)
+summary(oneway)
+oneway <- aov(stateUnderstanding ~ isExperimental, data = testData)
+summary(oneway)
+oneway <- aov(contributionUnderstanding ~ isExperimental, data = testData)
+summary(oneway)
+oneway <- aov(scopeFrequency ~ isExperimental, data = testData)
+summary(oneway)
+oneway <- aov(playtestFrequency ~ isExperimental, data = testData)
 summary(oneway)
 
-twoway <- aov(buildConfidence ~ isExperimental + id, data = testData)
-summary(twoway)
 
-interaction <- aov(buildConfidence ~ isExperimental * id, data = testData)
-summary(interaction)
 
-blocking <- aov(buildConfidence ~ isExperimental * id + team, data = testData)
-summary(blocking)
+# twoway <- aov(scopeConfidence ~ isExperimental + id, data = testData)
+# summary(twoway)
+
+# interaction <- aov(scopeConfidence ~ isExperimental * id, data = testData)
+# summary(interaction)
+
+# blocking <- aov(scopeConfidence ~ isExperimental * id + team, data = testData)
+# summary(blocking)
